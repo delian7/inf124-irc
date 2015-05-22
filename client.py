b8dedca759c9580d6bfb040d8a6ae6908adac2e3
Delian Petrov <petrovd@uci.edu>
2015-05-21 22:06:20 -0700

    Merge branch 'delian's-stuff-to-test'
    Conflicts:
    	client.py

--- client.py
+++ client.py
@@ -3,10 +3,6 @@ import sys
 from threading import Thread
 from time import sleep
 
-<<<<<<< HEAD
-agent_free = True
-=======
->>>>>>> 626b3d66b7f2377b9fe7000a9d03b6d55294a03b
 
 myname = raw_input('What is your name? ')
 
@@ -26,18 +22,7 @@ def periodic_poll():
     while 1:
         poll()
         sleep(0.05)  # seconds
-<<<<<<< HEAD
-                            
-=======
 
-
-def change_order():
-    order_number = raw_input("What is your order number?")
-
-
-
-
->>>>>>> 626b3d66b7f2377b9fe7000a9d03b6d55294a03b
 thread = Thread(target=periodic_poll)
 thread.daemon = True  # die when the main thread dies
 thread.start()
@@ -45,9 +30,13 @@ thread.start()
 while 1:
     mytxt = sys.stdin.readline().rstrip()
     options = {'1':'change order', '2':'cancel order', '3':'get ETA', '4':'ask general questions'}
+
+    if mytxt == ':e':
+        print 'lolololololol'
+    
     if mytxt in options:
         client.do_send({'option': options[mytxt], 'txt': mytxt})
-    elif mytxt == '5':
+    elif mytxt == '5' or mytxt.lower() == ':q':
         print "Closing connection to server."
         sys.exit()
     else:
--- server.py
+++ server.py
@@ -9,28 +9,33 @@ def createMenu():
 	option3 = "3: Get ETA"
 	option4 = "4: Ask general questions?"
 	option5 = "5: Exit"
-	
+
 	return opening + "\n" + option1 + "\n" + option2 + "\n" + option3 + "\n" + option4 + "\n" + option5
+
+def output_to_file():
+        pass
+
 
 class MyHandler(Handler):
 
 	def on_open(self):
-		pass
+                pass
 
 	def on_close(self):
 		pass
-		
+
 	def on_msg(self, msg):
 		if type(msg) is dict:
-			if 'join' in msg:
-				print msg
-				self.do_send('Hello ' + msg['join'] + '!\n' + createMenu())
-			elif 'option' in msg:
-				print msg
-				agentMessage = "Checking for available agent now..."
-				self.do_send('You would like to ' + msg['option'] + '. ' + agentMessage)
-					
-			
+            if 'join' in msg:
+                print msg
+                self.do_send('Hello ' + msg['join'] + '!\n' + createMenu())
+            elif 'option' in msg:
+                print msg
+                agentMessage = "Checking for available agent now..."
+                self.do_send('You would like to ' + msg['option'] + '. ' + agentMessage)
+		else
+            print msg
+
 class ServerListener(Listener):
 
 	#inherits from listener
@@ -45,6 +50,8 @@ class ServerListener(Listener):
 	def print_clients(self):
 		for item in self.connections:
 			print str(item[0]) + " " + str(item[1])
+
+
 
 port = 8888
 server = ServerListener(port, MyHandler)
